2e151fedabb57be50578abaaf24f46611b7faf31
moises <jimenezsmoises27@gmail.com>
2024-05-16 19:13:37 -0400

    se agregan cambios

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ while ! mysqladmin ping -h"db" --silent; do
     sleep 1
 done
 
+# Instalar dependencias de Composer
+composer install --no-interaction --no-plugins --no-scripts --prefer-dist
+
 # Ejecutar migraciones
 php artisan migrate --force
 
